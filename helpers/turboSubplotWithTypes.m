44d78ca48096d09da1d5f91aed4a372d913ca0bf
JacekJurek <62620912+JacekJurek@users.noreply.github.com>
2020-03-27 20:22:32 +0100

    Create turboSubplotWithTypes.m

---
+++ helpers/turboSubplotWithTypes.m
@@ -0,0 +1,25 @@
+function turboSubplotWithTypes(rows, columns, images, types, titles, filename)
+
+figure('units', 'normalized', 'outerposition', [0 0 1 1]);
+[x , ~] = size(images);
+
+for i=1:1:x
+    subplot(rows,columns,i);
+    switch types{i}
+        case 'image'
+            imshow(images{i});
+        case 'hist'
+            imhist(images{i});
+        case 'histeqImage'
+            imshow(histeq(images{i}));
+        case 'histeq'
+            imhist(histeq(images{i}));
+        otherwise
+            disp(['invalid type for i=' num2str(i) ]);
+    end
+    title(titles{i});
+end
+
+saveas(gcf,['Images\' filename '.png']);
+
+end
